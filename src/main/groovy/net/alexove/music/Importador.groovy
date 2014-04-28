99fb87c8c91060e6f98da1e28c0620a4b53b8adc
Alex Oviedo <alleinerwolf@gmail.com>
2014-04-28 12:13:17 -0500

    new file:   src/main/groovy/net/alexove/music/Importador.groovy 	new file:   src/test/groovy/net/alexove/music/ImportadorTest.groovy

---
+++ src/main/groovy/net/alexove/music/Importador.groovy
@@ -0,0 +1,8 @@
+package net.alexove.music
+
+public class Importador{
+    String dirOrigen
+    String dirDestino
+    
+    def LeerCancion(){}
+}
---
+++ src/test/groovy/net/alexove/music/ImportadorTest.groovy
@@ -0,0 +1,14 @@
+package net.alexove.music
+
+import org.junit.Test
+import static org.junit.Assert.assertEquals
+
+class ImportadorTest{
+     @Test
+     void testLeerCancion(){
+         String origen="/home/alexove/origen"
+	 String destino="/home/alexove/destino"
+
+	 assertEquals 2+2,4
+     }
+}
